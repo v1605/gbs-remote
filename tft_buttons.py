922fd63309a4e2e884a8a2f50b97c1847db382d4
v1605 <55302877+v1605@users.noreply.github.com>
2024-01-24 22:14:18 -0500

    Removed comments and changed to 13

--- tft_buttons.py
+++ tft_buttons.py
@@ -2,11 +2,11 @@ from machine import Pin
 
 class Buttons():
     def __init__(self):
-        self.name = "ws_pico_114"
-        self.key0 = Pin(15,Pin.IN,Pin.PULL_UP)  # KEY 0
-        self.key1 = Pin(17 ,Pin.IN,Pin.PULL_UP) # KEY 1
-        self.key2 = Pin(19,Pin.IN,Pin.PULL_UP)   # KEY 2
-        self.key3 = Pin(21,Pin.IN,Pin.PULL_UP)   # KEY 3
+        self.name = "ws_pico_13"
+        self.key0 = Pin(15,Pin.IN,Pin.PULL_UP)
+        self.key1 = Pin(17 ,Pin.IN,Pin.PULL_UP)
+        self.key2 = Pin(19,Pin.IN,Pin.PULL_UP)
+        self.key3 = Pin(21,Pin.IN,Pin.PULL_UP)
         self.up = Pin(18,Pin.IN,Pin.PULL_UP)
         self.down = Pin(2,Pin.IN,Pin.PULL_UP)
         self.left = Pin(16,Pin.IN,Pin.PULL_UP)
